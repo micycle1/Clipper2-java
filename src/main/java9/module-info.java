86a4af8b507d242733b93a4a8c4ff80201567e43
Claas j. Gramann <claas.j.gramann@gmail.com>
2023-09-17 17:12:59 +0200

    Add module-info.java as multi release

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,9 @@
+module clipper2 {
+    exports clipper2;
+    exports clipper2.core;
+    exports clipper2.engine;
+    exports clipper2.offset;
+    exports clipper2.rectclip;
+
+    exports tangible;
+}
