micycle1/Clipper2-java

commit d49b93b9cd0c441b7291d99555fbf1b6852e7773

562ef2d Another minor bugfix in GetIntersectPoint() [breaks few tests]

src/main/java/clipper2/core/InternalClipper.java
@@ -56,15 +56,24 @@ public final class InternalClipper {
 		double dx1 = (ln1b.x - ln1a.x);
 		double dy2 = (ln2b.y - ln2a.y);
 		double dx2 = (ln2b.x - ln2a.x);
-		double cp = dy1 * dx2 - dy2 * dx1;
-		if (cp == 0.0) {
+		double det = dy1 * dx2 - dy2 * dx1;
+		if (det == 0.0) {
+			ip = new Point64();
 			return false;
 		}
-		double qx = dx1 * ln1a.y - dy1 * ln1a.x;
-		double qy = dx2 * ln2a.y - dy2 * ln2a.x;
-		ip.x = CheckCastInt64((dx1 * qy - dx2 * qx) / cp);
-		ip.y = CheckCastInt64((dy1 * qy - dy2 * qx) / cp);
-		return (ip.x != Invalid64 && ip.y != Invalid64);
+
+		double t = ((ln1a.x - ln2a.x) * dy2 - (ln1a.y - ln2a.y) * dx2) / det;
+		if (t <= 0.0) {
+			ip = ln1a;
+		}
+		else if (t >= 1.0) {
+			ip = ln1b;
+		}
+		else {
+			ip.setX(ln1a.x + t * dx1);
+			ip.setY(ln1a.y + t * dy1);
+		}
+		return true;
 	}
 
 	public static boolean GetIntersectPoint(Point64 ln1a, Point64 ln1b, Point64 ln2a, Point64 ln2b, /* out */ Point64 ip) {

commit e8eaf72ce408d7dd27884bd8000dc84c33f0b7b3

Fix bug introduced in d49b93b

src/main/java/clipper2/core/InternalClipper.java
@@ -55,25 +55,32 @@ public final class InternalClipper {
 		double dx1 = (ln1b.x - ln1a.x);
 		double dy2 = (ln2b.y - ln2a.y);
 		double dx2 = (ln2b.x - ln2a.x);
+
 		double det = dy1 * dx2 - dy2 * dx1;
+
 		if (det == 0.0) {
-			ip.setX(0);
-			ip.setY(0);
+			ip.x = 0;
+			ip.y = 0;
 			return false;
 		}
 
+		// Calculate the intersection parameter 't' along the first line segment
 		double t = ((ln1a.x - ln2a.x) * dy2 - (ln1a.y - ln2a.y) * dx2) / det;
+
+		// Determine the intersection point based on 't'
 		if (t <= 0.0) {
-			ip = ln1a;
-		}
-		else if (t >= 1.0) {
-			ip = ln1b;
+			ip.x = ln1a.x;
+			ip.y = ln1a.y;
+		} else if (t >= 1.0) {
+			ip.x = ln1b.x;
+			ip.y = ln1b.y;
 		} else {
-			// NB: truncate the result instead of rounding it, to make the C# version work
-			// similarly to the C++ and Delphi versions
+			// avoid using constructor (and rounding too) as they affect performance //664
 			ip.x = (long) (ln1a.x + t * dx1);
 			ip.y = (long) (ln1a.y + t * dy1);
 		}
+
+		// Intersection found (even if clamped to endpoints)
 		return true;
 	}
 